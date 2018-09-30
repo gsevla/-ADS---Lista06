25e59709f32fa0a70ffa6884eec2ad5b2af6bc32
Gabriel Alves <gabrieltots@gmail.com>
2018-09-30 12:01:08 -0300

    DEU CERTO, DESGRAÇA, SEMPRE ACREDITEI! falta fazer a conta das colunas 7 a 9

--- t.py
+++ t.py
@@ -60,7 +60,7 @@ def createClients(q, qtd):
 def sorteio():
     #Vars
     tArrival = 0
-    tService = 0
+    tAttendance = 0
     tFila = 0
     tFinal = 0
     tBanco = 0
@@ -69,37 +69,45 @@ def sorteio():
     clients = queue.Queue(maxsize=qtdClients)
     createClients(clients, qtdClients)
     table = [['Clientes'], ['T. Últ. Chegada'], ['T. Chegada'], ['T. Serviço'], ['T. Início'], ['T. Fila'], ['T. Fim'], ['T. Banco'], ['T. C.L']]
+    
+    #cliente 1
+    c = clients.get() 
+    table[0].append(c.clientNumber)
+    table[1].append(c.lastArrival)
+    table[2].append((c.lastArrival))
+    tArrival += c.lastArrival
+    table[3].append(c.attendanceTime)
+    #tAttendance += c.attendanceTime
+    tAttendance += c.lastArrival
+    table[4].append(c.lastArrival)
+    table[5].append(0)
+    table[6].append(c.lastArrival+c.attendanceTime)
+    table[7].append(c.attendanceTime)
+    table[8].append(c.lastArrival)
+
+    print('    {}\t|    {}\t|\t{}\t|\t{}\t|\t{}\t|    {}\t|    {}\t|\t{}\t|\t {}\t'.format(table[0][0], table[1][0], table[2][0], table[3][0], table[4][0], table[5][0], table[6][0], table[7][0], table[8][0]))
+    print('|\t{}\t|\t    {}\t\t|\t    {}\t\t|\t    {}\t\t|\t    {}\t\t|\t{}\t|\t{}\t|\t    {}\t\t|\t    {}\t\t|'.format(table[0][1], table[1][1], table[2][1], table[3][1], table[4][1], table[5][1], table[6][1], table[7][1], table[8][1]))
+
+    #outros clientes
     for i in range(clients.qsize()):
         c = clients.get()
         table[0].append(c.clientNumber)
         table[1].append(c.lastArrival)
         tArrival += c.lastArrival
-        table[2].append((tArrival))
+        table[2].append(tArrival)
         table[3].append(c.attendanceTime)
-        if(c.clientNumber == 1):
-            tService = c.lastArrival
-            table[4].append(tService) # anterior + última chegada
-        else:
-            if((tService+c.lastArrival) >= (tService+c.attendanceTime)):
-                tService += c.lastArrival
-            else:
-                tService += c.attendanceTime
-            table[4].append(tService)
-        table[5].append(tService-tArrival)
-        if(c.clientNumber == 1):
-            tFinal = tService-tArrival
-            table[6].append(tService-tArrival)
-        else:
-            tFinal += c.attendanceTime
-            table[6].append(tFinal)
+        table[4].append(tArrival)
+        if(table[3][i+1] > table[1][i+2]):
+            #tAttendance += table[1][i+2]
+            table[4][i+2] = tArrival+(table[3][i+1]-table[1][i+2])
+        table[5].append(table[4][i+2]-table[2][i+2])
+        table[6].append(0)
         table[7].append(0)
         table[8].append(0)
-
-    print('    {}\t|    {}\t|\t{}\t|\t{}\t|\t{}\t|    {}\t|    {}\t|\t{}\t|\t {}\t'.format(table[0][0], table[1][0], table[2][0], table[3][0], table[4][0], table[5][0], table[6][0], table[7][0], table[8][0]))
     
-    for i in range(qtdClients):
-        print('|\t{}\t|\t    {}\t\t|\t    {}\t\t|\t    {}\t\t|\t    {}\t\t|\t{}\t|\t{}\t|\t    {}\t\t|\t    {}\t\t|'.format(table[0][i+1], table[1][i+1], table[2][i+1], table[3][i+1], table[4][i+1], table[5][i+1], table[6][i+1], table[7][i+1], table[8][i+1]))
-    print('\t\t\t\t\t\t\t\tTotal: {} \t\t\t\t\tTotal: {} \t\t\tTotal: {} \t\tTotal: {}'.format(tService, tFila, tBanco, tLivre))
+    for i in range(len(table[0])-2):
+        print('|\t{}\t|\t    {}\t\t|\t    {}\t\t|\t    {}\t\t|\t    {}\t\t|\t{}\t|\t{}\t|\t    {}\t\t|\t    {}\t\t|'.format(table[0][i+2], table[1][i+2], table[2][i+2], table[3][i+2], table[4][i+2], table[5][i+2], table[6][i+2], table[7][i+2], table[8][i+2]))
+    print('\t\t\t\t\t\t\t\tTotal: {} \t\t\t\t\tTotal: {} \t\t\tTotal: {} \t\tTotal: {}'.format(tAttendance, tFila, tBanco, tLivre))
 
 
 def main():
